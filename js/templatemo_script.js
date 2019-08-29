6a3b641cc30f762f79d49b29cb08677bb0f346ab
IT FG _ Umar <itsmafuturegate@gmail.com>
2019-08-30 03:38:53 +0700

    adding content

--- js/templatemo_script.js
+++ js/templatemo_script.js
@@ -21,35 +21,43 @@ jQuery(function($) {
         var defaultImgSrc = $('img.main-img').attr('src');
         $.backstretch(defaultImgSrc, {speed: 400});
 
-	//for image slide on menu item click(normal) and responsive
-	$(".change-section").on('click',function(e){
-            e.preventDefault();
-            if (menuDisabled == false) // check the menu has disabled?
-            {
-                menuDisabled = true; // disable to menu
-                
-                var name = $(this).attr('href');
-                // console.log(name);
-                // get image url and assign to backstretch for background
-                var imgSrc = $("img"+name+"-img").attr('src');
-                $.backstretch(imgSrc, {speed: 400}); //backstretch for background fade in/out
-                
-                // content zoom in/out
-                $("section.active").hide('size',{easing: 'easeInQuart', duration: 400},function(){
-                    $(this).removeClass("active");
-                    $(name+"-section").show('size',{easing: 'easeOutQuart', duration: 400},function(){
-                        $(this).addClass("active");
-                        
-                        // google map need to resize for animate contents
-                        // google.maps.event.trigger(map, 'resize'); // resize map
-                        $.backstretch("resize"); // resize the background image
-                        menuDisabled = false;
-                     });
-                });                
-            }
-            return;
-	});
+    	//for image slide on menu item click(normal) and responsive
+    	$(".change-section").on('click',function(e){
+                e.preventDefault();
+                if (menuDisabled == false) // check the menu has disabled?
+                {
+                    menuDisabled = true; // disable to menu
+                    
+                    var name = $(this).attr('href');
+                    // console.log(name);
+                    // get image url and assign to backstretch for background
+                    var imgSrc = $("img"+name+"-img").attr('src');
+                    $.backstretch(imgSrc, {speed: 400}); //backstretch for background fade in/out
+                    
+                    // content zoom in/out
+                    $("section.active").hide('size',{easing: 'easeInQuart', duration: 400},function(){
+                        $(this).removeClass("active");
+                        $(name+"-section").show('size',{easing: 'easeOutQuart', duration: 400},function(){
+                            $(this).addClass("active");
+                            
+                            // google map need to resize for animate contents
+                            // google.maps.event.trigger(map, 'resize'); // resize map
+                            $.backstretch("resize"); // resize the background image
+                            menuDisabled = false;
+                         });
+                    });                
+                }
+                return;
+    	});
         
+
+        console.log('tautan', location.search);
+
+        if(location.search == '?tautan'){
+            console.log('masuk tautan');
+            $('#menu-section').removeClass('active').addClass('inactive');
+            $('#tautan-section').removeClass('inactive').addClass('active');
+        }
     });
 
 });
